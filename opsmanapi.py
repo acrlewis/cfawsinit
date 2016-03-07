52259697983470689b1b52a270f3f0e0dc6a36bc
Mandar U Jog <mandarjog@gmail.com>
2016-03-07 05:31:25 -0500

    add yml driven install

--- opsmanapi.py
+++ opsmanapi.py
@@ -1,77 +1,120 @@
 from bs4 import BeautifulSoup
 from robobrowser import RoboBrowser
+from robobrowser import forms
+import yaml
 
 
 class OpsManApi(object):
-    URIs = [("infrastructure/iaas_configuration", {u'utf': u'utf8', u'authenticity_toke': u'authenticity_token', 
-        u'encrypted': u'iaas_configuration[encrypted]', 
-        u'region': u'iaas_configuration[region]', 
-        u'secret_access_key': u'iaas_configuration[secret_access_key]', 
-        u'_metho': u'_method', u'key_pair_name': u'iaas_configuration[key_pair_name]', 
-        u'ssh_private_key': u'iaas_configuration[ssh_private_key]', 
-        u'access_key_id': u'iaas_configuration[access_key_id]', 
-        u'security_group': u'iaas_configuration[security_group]', 
-        u'vpc_id': u'iaas_configuration[vpc_id]'}),
-            ("infrastructure/director_configuration", {u'access_key': u'director_configuration[s3_blobstore_options][access_key]', u'password': u'director_configuration[external_database_options][password]', u'endpoint': u'director_configuration[s3_blobstore_options][endpoint]', u'utf': u'utf8', u'database': u'director_configuration[external_database_options][database]', u'authenticity_toke': u'authenticity_token', u'user': u'director_configuration[external_database_options][user]', u'host': u'director_configuration[external_database_options][host]', u'metrics_ip': u'director_configuration[metrics_ip]', u'port': u'director_configuration[external_database_options][port]', u'_metho': u'_method', u'bucket_name': u'director_configuration[s3_blobstore_options][bucket_name]', u'blobstore_type': u'director_configuration[blobstore_type]', u'secret_key': u'director_configuration[s3_blobstore_options][secret_key]', u'ntp_servers_string': u'director_configuration[ntp_servers_string]', u'database_type': u'director_configuration[database_type]', u'max_threads': u'director_configuration[max_threads]', u'resurrector_enabled': u'director_configuration[resurrector_enabled]'}),
-            ("infrastructure/availability_zones", {u'authenticity_toke': u'authenticity_token', u'_metho': u'_method', u'guid': u'availability_zones[availability_zones][][guid]', u'utf': u'utf8', u'iaas_identifier': u'availability_zones[availability_zones][][iaas_identifier]'}),
-            ("infrastructure/director/availability_zone_assignment", {u'authenticity_toke': u'authenticity_token', u'_metho': u'_method', u'singleton_availability_zone': u'director_availability_zone_assignments[singleton_availability_zone]', u'utf': u'utf8'}),
-            ("infrastructure/networks", {u'subnet': u'network[networks][][subnet]', u'utf': u'utf8', u'name': u'network[networks][][name]', u'authenticity_toke': u'authenticity_token', u'iaas_network_identifier': u'network[networks][][iaas_network_identifier]', u'_metho': u'_method', u'reserved_ip_ranges': u'network[networks][][reserved_ip_ranges]', u'dns': u'network[networks][][dns]', u'guid': u'network[networks][][guid]', u'gateway': u'network[networks][][gateway]'}),
-            ("infrastructure/director/single_network_assignment", {u'authenticity_toke': u'authenticity_token', u'_metho': u'_method', u'utf': u'utf8', u'network': u'director_single_network_assignments[network]'})]
-
-    def __init__(self, url, username, password, private_key_file, stack_vars):
+    def __init__(self, url, username, password, private_key_file,
+                 stack_vars, region='us-east-1'):
         self.url = url
         self.username = username
         self.password = password
-        self.private_key_file = private_key_file
+        self.private_key = open(private_key_file, "rt").read()
         self.browser = RoboBrowser(history=True)
         self.var = stack_vars
+        self.region = region
+        if region == 'us-east-1':
+            self.s3_endpoint = "https://s3.amazonaws.com"
+        else:
+            self.s3_endpoint = "https://s3-{}.amazonaws.com".format(region)
 
     def login(self):
-        self.browser.open(self.url+"/login", verify=False)
+        self.browser.open(self.url + "/login", verify=False)
         form = self.browser.get_form(action='/login')
         form['login[user_name]'].value = self.username
         form['login[password]'].value = self.password
         self.browser.submit_form(form)
         if self.browser.response.status_code >= 400:
-            raise Exception("Error login in {}\n{}".format(self.username,
-                            self.browser.response.text))
+            raise Exception("Error login in {}\n{}".
+                            format(self.username, self.browser.response.text))
 
     def process_action(self, action, mappings):
-        self.browser.open(self.url+"/"+action+"/edit", verify=False)
-        form = self.browser.get_form(action='/'+action)
-        # forms use ruby hash style params
-        fields = {k.rpartition('[')[-1][:-1]: k for k in form.keys()}
-        print fields
-        for k, v in mappings.items():
-            if k in fields:
-                form[fields[k]].value = v
 
+        form = None
+        for suffix in ["/new", "/edit"]:
+            self.browser.open(self.url + "/" + action + suffix, verify=False)
+            form = self.browser.get_form(action='/' + action)
+            if form is not None:
+                break
+
+        if form is None:
+            raise Exception("Could not find form for action="+action)
+
+        # forms use ruby hash style params
+        print form
+        for k, v in mappings.items():
+            if k.startswith("__"):
+                continue
+            form[k].value = v
+            print k, "=", v
+
+        print form
         self.browser.submit_form(form)
         soup = BeautifulSoup(self.browser.response.text)
         # check if errors-block class is there in the output
+        # ops manager sometime returns a 200 with and errors block
+        # in html
         # HACK warning
         errblock = soup.select('.errors-block')
-        if len(errblock) > 0:
-            raise Exception("Error while processing {}\n{}".
-                            format(action, str(errblock[0])))
 
-    def _make_form_mappings(self, vmappings, mappings):
-        ret = {}
-        ret.update(mappings)
-        ret.update({k: self.var[v] for k, v in vmappings.items()})
-        return ret
+        if self.browser.response.status_code >= 400 or len(errblock) > 0:
+            if '__IGNORE_ERROR__' not in mappings or \
+                    mappings['__IGNORE_ERROR__'] not in errblock[0].text:
+                raise Exception("Error submitting form " +
+                                self.browser.response.text)
 
-    def process_iaas_configuration(self):
-        vmappings = {"access_key_id": "PcfIamUserAccessKey",
-                "secret_access_key": "PcfIamUserSecretAccessKey",
-                "vpc_id": "PcfVpc",
-                "security_group": "PcfVmsSecurityGroupName",
-                "key_pair_name": "PcfKeyPairName"}
-        mappings = {
-                "region": "us-east-1",
-                "ssh_private_key": open(self.private_key_file, "rt").read()}
-        self.process_action("infrastructure/iaas_configuration",
-                self._make_form_mappings(vmappings, mappings))
+    def load_mappings(self, filename):
+        """
+        load mappings and hydrate using self, stack_vars
+        """
+        mappings = yaml.load(open(filename, 'rt'))
+        for mapping in mappings:
+            mp = mapping.values()[0]
+            for key, val in mp.items():
+                if isinstance(val, (bool, int, long)):
+                    continue
+                if val.startswith("$."):
+                    attrib = val[2:]
+                    if hasattr(self, attrib):
+                        mp[key] = getattr(self, attrib)
+                    else:
+                        raise Exception(val + " Is not provided"
+                                        " as a mapping variable")
+                elif val.startswith("$"):
+                    attrib = val[1:]
+                    if attrib in self.var:
+                        mp[key] = self.var[attrib]
+                    else:
+                        raise Exception(val + " Is not provided"
+                                        " as a stack output variable")
+        return mappings
+
+    def process_mappings(self, filename, action=None):
+        mappings = self.load_mappings(filename)
+        for mapping in mappings:
+            ac, mp = mapping.items()[0]
+            if action is None or action == ac:
+                self.process_action(ac, mp)
+
+    def apply_changes(self):
+        self.browser.open(self.url, verify=False)
+        soup = BeautifulSoup(self.browser.response.text)
+        fx = soup.find('meta', {"name": 'csrf-token'})
+        csrf_token = fx.attrs["content"]
+
+        rsp = self.browser.session.put(self.url+"/install",
+                                       data={'authenticity_token': csrf_token})
+
+        if rsp.status_code == 422 and \
+                'Ignore errors and start the install' in rsp.text:
+            # This happens because of the icmp error
+            sp = BeautifulSoup(rsp.text)
+            inst_form = sp.find("form", {"action": "/install"})
+            if inst_form is None:
+                raise Exception("Unable to complete installation")
+            self.browser.submit_form(forms.form.Form(inst_form))
+
 
 """
 In [218]: ops1['PcfVmsSecurityGroupName'] = list(ec2.security_groups.filter(GroupIds=[ops1['PcfVmsSecurityGroupId']]))[0].group_name
